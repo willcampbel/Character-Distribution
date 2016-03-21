7f1bac0bbb2a16f9bdf70acab7e2c3fc408c7f69
willcampbel <williamcampbell@hanovernorwichschools.org>
2016-03-21 14:33:02 -0400

    Updated from Brython Server: 3/21/2016 2:33:03 PM

--- distribution.py
+++ distribution.py
@@ -40,8 +40,8 @@ string = input("Please enter a string of text (the bigger the better): ").lower(
 list = str.split(string) 
 alpha=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 freq=[]
-for x in len(alpha)+1:
-    freq.append()
+for x in len(alpha):
+    freq.append("")
 print(freq)
 
 for x in list:
