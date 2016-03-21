6e21387a1dfae2bf1dc43823f8e44cf363d4062e
willcampbel <williamcampbell@hanovernorwichschools.org>
2016-03-21 14:13:50 -0400

    Updated from Brython Server: 3/21/2016 2:13:51 PM

--- distribution.py
+++ distribution.py
@@ -36,3 +36,29 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
+f compare(a, b):
+    """
+    compare - generic comparison function for testing two elements.
+    """
+    return b > a
+
+
+def bsort(seq, cmp):
+    """
+    bsort - simple sorting algorithm that uses any comparison function
+    seq - a list to be sorted
+    cmp - a function for comparing two elements of seq
+    """
+    sorted = False  # assume the seq is not sorted to start with
+    while not sorted:
+        sorted = True   # assume it's already sorted correctly
+        for index, value in enumerate(seq): # for every element in seq
+            if index > 0:                   # past the first..
+                if not cmp(seq[index-1], value):  # if this element is out of order
+                    sorted = False          # then the list is not sorted yet
+                    seq[index-1], seq[index] = seq[index], seq[index-1] # and swap it
+
+
+tosort = [4, 10, 3, -1000, 30]
+bsort(tosort, compare)
+print(tosort)
